842618f73b2fd4a48381ff46b417b5a76026a92e
sagarrakshe <sagarrakshe2@gmail.com>
2025-04-17 16:32:42 +0530

    Add split logic and allowed values custom logic

--- main.go
+++ main.go
@@ -5,45 +5,77 @@ import (
 	"encoding/base64"
 	"fmt"
 	"net/http"
+	"slices"
+	"strings"
 )
 
 type Config struct {
-	HeaderName string `json:"headerName,omitempty"`
+	HeaderName      string   `json:"headerName,omitempty"`
+	HeaderSeparator string   `json:"headerSeparator,omitempty"`
+	AllowedValues   []string `json:"allowedValues,omitempty"`
 }
 
 func CreateConfig() *Config {
 	return &Config{
-		HeaderName: "X-Custom-Header",
+		HeaderName:      "X-Custom-Header",
+		HeaderSeparator: ":",
+		AllowedValues:   []string{},
 	}
 }
 
 type HeaderDecode struct {
-	next       http.Handler
-	name       string
-	headerName string
+	next            http.Handler
+	name            string
+	headerName      string
+	headerSeparator string
+	allowedValues   []string
 }
 
-func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	return &HeaderDecode{
-		next:       next,
-		name:       name,
-		headerName: config.HeaderName,
-	}, nil
+func New(_ context.Context, config *Config, _ string) (http.Handler, error) {
+	if config.HeaderName == "" {
+		return nil, fmt.Errorf("headerName is required")
+	}
+	if config.HeaderSeparator == "" {
+		config.HeaderSeparator = ":"
+	}
 
+	return &HeaderDecode{
+		headerName:      config.HeaderName,
+		headerSeparator: config.HeaderSeparator,
+		allowedValues:   config.AllowedValues,
+	}, nil
 }
 
 func (h *HeaderDecode) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	encoded := req.Header.Get(h.headerName)
-	if encoded != "" {
-		decoded, err := base64.StdEncoding.DecodeString(encoded)
-		if err != nil {
-			fmt.Printf("[plugin-%s] Failed to decode header %s: %v\n", h.name, h.headerName, err)
-		} else {
-			fmt.Printf("[plugin-%s] Decoded header %s: %s\n", h.name, h.headerName, string(decoded))
-		}
-	} else {
-		fmt.Printf("[plugin-%s] Header %s not found\n", h.name, h.headerName)
+	encodedHeader := req.Header.Get(h.headerName)
+
+	if encodedHeader == "" {
+		fmt.Println("Encoded header not found")
+		h.next.ServeHTTP(rw, req)
+		return
 	}
+
+	decoded, err := base64.StdEncoding.DecodeString(encodedHeader)
+	if err != nil {
+		fmt.Printf("Failed to decode header: %v", err)
+		http.Error(rw, "Invalid base64 header", http.StatusBadRequest)
+		return
+	}
+	parts := strings.SplitN(string(decoded), h.headerSeparator, 2)
+	if len(parts) != 2 {
+		fmt.Println("Decoded header format incorrect")
+		http.Error(rw, "Invalid header format", http.StatusBadRequest)
+		return
+	}
+
+	value := strings.TrimSpace(parts[1])
+
+	if len(h.allowedValues) > 0 && !slices.Contains(h.allowedValues, value) {
+		fmt.Printf("Value %s not in allowed list", value)
+		http.Error(rw, "Forbidden", http.StatusForbidden)
+		return
+	}
+	fmt.Printf("Value %s present in allowed list", value)
 
 	h.next.ServeHTTP(rw, req)
 }
